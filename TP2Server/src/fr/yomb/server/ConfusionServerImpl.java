efd855be048effbd55293ae341ce36f6181048a4
yombb <yomb51@hotmail.com>
2025-02-03 13:58:05 +0100

    Create ConfusionServerImpl.java

---
+++ TP2Server/src/fr/yomb/server/ConfusionServerImpl.java
@@ -0,0 +1,24 @@
+package fr.yomb.server;
+
+public class ConfusionServerImpl implements ConfusionServer {
+
+	public short[] zShortToShort(short[] src) {
+		// TODO Auto-generated method stub
+		return src;
+	}
+
+	public byte[] zByteToShort(byte[] src) {
+		return src;
+	}
+	
+	public short shortToZShort(short src) {
+		// TODO Auto-generated method stub
+		return src;
+	}
+
+	public Object objectToShort(Object src) {
+		// TODO Auto-generated method stub
+		return src;
+	}
+
+}
